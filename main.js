57b74cdd9e441be053e52b7e8c9a5bbb51ea9b6b
Benjamin Deutsch <Benjaneer@users.noreply.github.com>
2023-05-31 18:33:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import {SingleSheet} from "./sheets/single-sheet.js";
+import {SingleSheetWorn} from "./sheets/single-sheet-worn.js";
+import {SingleSheetRagged} from "./sheets/single-sheet-ragged.js";
 
 Hooks.on("ready", () => {
     /* Use this method at hook "ready" to register your journal style. */
@@ -10,5 +12,32 @@ Hooks.on("ready", () => {
         /* Key-identifier of the string for translation. */
         label: "STORYTELLER_ADDON_SINGLE.SingleSheet"
     })
+    /* Use this method at hook "ready" to register your journal style. */
+    game.StoryTeller.registerAddonSheet({
+        /* Unique key, must not overlap with other keys. It is used to access the object. */
+        key: "worn",
+        /* The class that implements the settings for the new journal. You don't need to create an instance, the class description itself is passed. */
+        sheet: SingleSheetWorn,
+        /* Key-identifier of the string for translation. */
+        label: "STORYTELLER_ADDON_SINGLE.SingleSheetWorn"
+    })
+    /* Use this method at hook "ready" to register your journal style. */
+    game.StoryTeller.registerAddonSheet({
+        /* Unique key, must not overlap with other keys. It is used to access the object. */
+        key: "ragged",
+        /* The class that implements the settings for the new journal. You don't need to create an instance, the class description itself is passed. */
+        sheet: SingleSheetRagged,
+        /* Key-identifier of the string for translation. */
+        label: "STORYTELLER_ADDON_SINGLE.SingleSheetRagged"
+    })
     console.log("Storyteller addon singlesheet | Ready")
-})
+});
+
+Handlebars.registerHelper("offset", function(value)
+{
+    return parseInt(value) + 1;
+});
+
+Handlebars.registerHelper('getIdByIndex', function(array, index) {
+    return array[index].id;
+});
